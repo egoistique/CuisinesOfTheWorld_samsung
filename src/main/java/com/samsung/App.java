962d56b2293350ff8eb907e9e5283808d036f1ff
katya <katya@example.ru>
2022-05-20 15:50:51 +0300

    lect 3 loquibase

--- src/main/java/com/samsung/App.java
+++ src/main/java/com/samsung/App.java
@@ -1,9 +1,12 @@
 package com.samsung;
 
+import org.h2.tools.Console;
 import org.springframework.boot.ConfigurableBootstrapContext;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
+
+import java.sql.SQLException;
 
 @SpringBootApplication
 public class App {
@@ -11,6 +14,10 @@ public class App {
 
         ConfigurableApplicationContext context = SpringApplication.run(App.class, args);
 
-
+        try {
+            Console.main(args);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
